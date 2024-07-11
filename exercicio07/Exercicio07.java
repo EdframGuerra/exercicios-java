83ad6272e5fabbfda74ca024b1f4478a45c61b5d
Edfram-Guerra <edfram.guerra@gmail.com>
2024-07-11 10:41:10 -0300

    .refazendo...

--- exercicio07/Exercicio07.java
+++ exercicio07/Exercicio07.java
@@ -2,8 +2,6 @@ package exercicio07;
 
 public class Exercicio07 {
 
-    public int idade;
-    public String nome;
-    public double altura;
+    
 
 }
--- exercicio07/ProgramExec007.java
+++ exercicio07/ProgramExec007.java
@@ -9,22 +9,7 @@ public class ProgramExec007 {
         Locale.setDefault(Locale.US);
         Scanner leitor = new Scanner(System.in);
 
-        System.out.print("informe seu nome e Sobre nome: ");
-        String nome = leitor.nextLine();
-      
-       System.out.print("Informe sua idade: ");
-       int idade = leitor.nextInt();
-
-
-       System.out.print("Informe sua altura: ");
-       double altura = leitor.nextDouble();
-
-       if(idade<10 || altura < 1.40){
-        System.out.println(nome + ", Acesso proibido!");
-       }
-       else{
-        System.out.println(nome + ", Acesso permitido!");
-       }
+        
 
         leitor.close();
     }
