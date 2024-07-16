9f67bc9bd33e1989d3a9f54b12f6d8b3153d1785
Edfram-Guerra <edfram.guerra@gmail.com>
2024-07-16 15:17:56 -0300

    Exercicio 08 finalizado

---
+++ exercicio08/ProgramExec008.java
@@ -0,0 +1,51 @@
+package exercicio08;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ProgramExec008 {
+
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner leitor = new Scanner(System.in);
+
+        System.out.print("Digite o primeiro valor: ");
+        int valorA = leitor.nextInt();
+
+        System.out.print("Digite o segundo valor: ");
+        int valorB = leitor.nextInt();
+
+        System.out.print("Digite o terceiro valor: ");
+        int valorC = leitor.nextInt();
+
+        if (valorA > valorB && valorA > valorC) {
+            if (valorB > valorC) {
+                System.out.println(valorA + ", " + valorB + ", " + valorC);
+            }
+        }
+        if (valorB > valorA && valorB > valorC) {
+            if (valorA < valorC) {
+                System.out.println(valorB + " , " + valorA + ", " + valorC);
+            }
+        } else if (valorA > valorC && valorA > valorB) {
+            if (valorC > valorB) {
+                System.out.println(valorA + " , " + valorC + ", " + valorB);
+            }
+        }
+        else if(valorB > valorC && valorB > valorA) {
+            if (valorC > valorA) {
+                System.out.println(valorB + " , " + valorC + ", " + valorA);
+            }
+        }
+        else if(valorC > valorA && valorC > valorB) {
+            if (valorA > valorB) {
+                System.out.println(valorC + " , " + valorA + ", " + valorB);
+            }
+        }else{
+            System.out.println(valorC + " , " + valorB + ", " + valorA);
+        }
+
+        leitor.close();
+
+    }
+}
