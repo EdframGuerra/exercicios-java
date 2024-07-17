4bc0d3eac1e7137e6c9e673874e47c32148884a2
Edfram-Guerra <edfram.guerra@gmail.com>
2024-07-17 11:14:59 -0300

    Exercicio finalizado

--- exercicio15/ProgramTempoVivido.java
+++ exercicio15/ProgramTempoVivido.java
@@ -1,5 +1,6 @@
 package exercicio15;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.ZoneId;
@@ -8,11 +9,11 @@ import java.util.Scanner;
 
 public class ProgramTempoVivido {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException{
         Scanner leitor = new Scanner(System.in);
 
         System.out.println("Informe sua data de nascimento (dd/mm/aaaa): ");
-        String dataNascimento = leitor.nextLine();
+                String dataNascimento = leitor.nextLine();
         SimpleDateFormat dataFormatada = new SimpleDateFormat("dd/MM/yyyy");
         Date dataDeNascimento = dataFormatada.parse(dataNascimento);
 
